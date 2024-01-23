cbfe3141198953defb44d597d281430096dd67cb
chathura-de-silva <ac.chathura@gmail.com>
2024-01-23 17:19:50 +0530

    Basic code

--- Birds.py
+++
@@ -1,3 +0,0 @@
-def BirdAtATime(row):
-    birds = {}
-    return birds
--- ChecklistGenerator.py
+++ ChecklistGenerator.py
@@ -1,3 +1,23 @@
-import pylatex
+import pandas as pd
+from pylatex import Enumerate, Itemize
+
+df = pd.read_excel('./uomBirds.xlsx')
+dfgrouped = df.groupby("Family")
+
+for group_name, group_data in dfgrouped:
+    # Process each group_data as needed
+    print(f"Group Name: {group_name}")
+    print(group_data)
+    # Perform your operations on the group_data
+      
+    
+        
+
+ 
 
 
+with open("./LatexCode.tex", "a") as tex_file:
+    tex_file.write(firstLevelList.dumps())
+
+
+    
